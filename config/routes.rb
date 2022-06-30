beb551c8e49ce7158b452c19ff072fe200e71723
JeongminJamie <sksrhcalsu@gmail.com>
2022-06-30 13:49:32 -0400

    fixing for admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,11 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    
+    resources :categories
   end
 
-
-  get '/about', to: 'about#index'
+  resources :about
+  # get '/about', to: 'about#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
